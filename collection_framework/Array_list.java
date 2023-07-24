2f8572a799362fc524ef1c2372e2c42161c8d284
sudhanshu shubham <shubhamsudhanshu398@gmail.com>
2023-07-24 12:34:39 +0530

    java Array_list

--- collection_framework/Array_list.java
+++ collection_framework/Array_list.java
@@ -100,3 +100,28 @@ class Main {
     System.out.println("Removed Element: " + str);
   }
 }
+
+
+// Iterate through an ArrayList
+// We can use the Java for-each loop to loop through each element of the arraylist. For example,
+
+
+class Main {
+  public static void main(String[] args) {
+
+    // creating an array list
+    ArrayList<String> animals = new ArrayList<>();
+    animals.add("Cow");
+    animals.add("Cat");
+    animals.add("Dog");
+    System.out.println("ArrayList: " + animals);
+
+    // iterate using for-each loop
+    System.out.println("Accessing individual elements:  ");
+
+    for (String language : animals) {
+      System.out.print(language);
+      System.out.print(", ");
+    }
+  }
+}
